e1f3a805a0e1418567f73268a2a970650d0bbdf1
Mattia311 <mattia.space98@gmail.com>
2021-11-25 10:28:55 +0100

    Add components

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,27 @@
 <template>
   <div id="app">
+    <SiteHeader/>
+    <SiteMain/>
+    <SiteFooter/>
+    
   </div>
 </template>
 
 <script>
-
+import SiteHeader from './components/SiteHeader.vue';
+import SiteMain from './components/SiteMain.vue';
+import SiteFooter from './components/SiteFooter.vue';
 
 export default {
   name: 'App',
   components: {
-    
+    SiteHeader,
+    SiteMain,
+    SiteFooter,
   }
 }
 </script>
 
 <style lang="scss">
+@import "../node_modules/bootstrap/scss/bootstrap.scss";
 </style>
