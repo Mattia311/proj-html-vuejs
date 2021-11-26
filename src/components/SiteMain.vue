413091b13ae1a726b33e133f211cbd3d0407a744
Mattia311 <mattia.space98@gmail.com>
2021-11-26 15:21:15 +0100

    Main completo

--- src/components/SiteMain.vue
+++ src/components/SiteMain.vue
@@ -65,25 +65,164 @@
                   <div class="main_third_top_right">
                       <p class="meggy">MEGGY STEWART</p>
                       <h2 class="title_new_york">NEW YORK TIMES</h2>
-                      <p class="stars"><i class="fas fa-star"></i><i class="fas fa-star"></i><i class="fas fa-star"></i><i class="fas fa-star"></i><i class="fas fa-star"></i></p>
+                      <p class="stars"><i class="fas fa-star"></i>&nbsp;<i class="fas fa-star"></i>&nbsp;<i class="fas fa-star"></i>&nbsp;<i class="fas fa-star"></i>&nbsp;<i class="fas fa-star"></i></p>
                       <div class="line_main_third"></div>
                       <p class="text_main_top_right">Non arcu mauris tortor ultrices mollis tellus <br>euismod fermentum. Habitant amet tincidunt <br>id sapien accumsan sed at. </p>
                       <p class="text_main_top_right">Mmalesuada ullamcorper amet. Pretium <br>pretium dignissim nisi.</p>
-                      <button class="button_main_top_tight">READ FULL ARTICLE</button>
+                      <button class="button_main_top_right">READ FULL ARTICLE</button>
                   </div>
               </div>
            </div>
        </div>
        <div class="main_fourth d-flex">
            <div class="main_fourth_left">
-               <img src="../assets/images/info52x.jpg" alt="">
-
-           </div>
+               <div class="main_fourth_img_left">
+                   <img src="../assets/images/info52x.jpg" alt="">
+               </div>
+               <div class="main_fourth_text">
+                   <p>MARY MAXEY</p>
+                   <h2>THE GUARDIAN</h2>
+                   <p class="stars"><i class="fas fa-star"></i>&nbsp;<i class="fas fa-star"></i>&nbsp;<i class="fas fa-star"></i>&nbsp;<i class="fas fa-star"></i>&nbsp;<i class="fas fa-star"></i></p>
+                   <div class="line_main_third"></div>
+                   <p class="text_main_top_right">Non arcu mauris tortor ultrices mollis tellus <br>euismod fermentum. Habitant amet tincidunt <br>id sapien accumsan sed at. </p>
+                   <button class="button_main_top_right">READ FULL ARTICLE</button>
+                </div>
+          </div>
            <div class="main_fourth_right">
-               <img src="../assets/images/info12x.jpg" alt="">
-
+               <div class="main_fourth_img_right">
+                   <img src="../assets/images/info12x.jpg" alt="">
+               </div>
+               <div class="main_fourth_text">
+                   <p>PATRICK MONROE</p>
+                   <h2>GLOBE AND MAIL</h2>
+                   <p class="stars"><i class="fas fa-star"></i>&nbsp;<i class="fas fa-star"></i>&nbsp;<i class="fas fa-star"></i>&nbsp;<i class="fas fa-star"></i>&nbsp;<i class="fas fa-star"></i></p>
+                   <div class="line_main_third"></div>
+                   <p class="text_main_top_right">Non arcu mauris tortor ultrices mollis tellus <br>euismod fermentum. Habitant amet tincidunt <br>id sapien accumsan sed at. </p>
+                   <button class="button_main_top_right">READ FULL ARTICLE</button>
+                </div>
+          </div>
+       </div>
+       <div class="main_five">
+           <img src="../assets/images/page52x.jpg" alt="">
+           <div class="main_five_text">
+               <p>KUNG PAO CHICKEN - $32</p>
            </div>
        </div>
+       <div class="main_six">
+           <h2 class="text-center">Lates news e promotions</h2>
+           <div class="container main_six_images">
+               <div class="row">
+                   <div class="col-4 text-center">
+                       <img src="../assets/images/blog92x.jpg" alt="">
+                       <h3>THE BEST COFFEE IN TOWN</h3>
+                       <p>By admin | january 7th, 2020 | Categories: News</p>
+                   </div>
+                   <div class="col-4 text-center">
+                       <img src="../assets/images/blog72x.jpg" alt="">
+                       <h3>DISCOVER OUR NEW MENU</h3>
+                       <p>By admin | january 7th, 2020 | Categories: News</p>
+                    </div>
+                   <div class="col-4 text-center">
+                       <img src="../assets/images/blog82x.jpg" alt="">
+                       <h3>WE NOW ACCEPT SQUARE!</h3>
+                       <p>By admin | january 7th, 2020 | Categories: News</p>
+                    </div>
+               </div>
+           </div>
+        </div>
+        <div class="main_seven d-flex row">
+            <div class="col-7">
+                <img src="../assets/images/slider72x-scaled.jpg" alt="">
+            </div>
+            <div class="col-5">
+                <div class="main_seven_text_top d-flex">
+                    <div class="line"></div>
+                    <p>ENJOY YOUR MEAL AT HOME</p>
+                </div>
+                    <h2>TAKEOUT NOW <br>AVALAIBLE</h2>
+                    <p class="text_main_top_right">Non arcu mauris tortor ultrices mollis tellus <br>euismod fermentum. Habitant amet tincidunt <br>id sapien accumsan sed at. </p>
+                    <button><i class="fas fa-car"></i>&nbsp;&nbsp;VIEW TAKEOUT MENU</button>
+            </div>
+        </div>
+        <div class="main_eight">
+            <div class="container">
+                <div class="row">
+                    <div class="col-4 main_eight_one">
+                        <h2>HORS D'OEUVRES</h2>
+                        <div class="d-flex">
+                            <h4>AHI SALMON NIGIRI</h4>
+                            <p>$48</p>
+                        </div>
+                        <p>Eget vulputate vitae quis rutrum blandit <br>sed. Quam nulla sit lacinia.</p>
+                        <div class="d-flex">
+                            <h4>AHI SALMON NIGIRI</h4>
+                            <p>$48</p>
+                        </div>
+                        <p>Eget vulputate vitae quis rutrum blandit <br>sed. Quam nulla sit lacinia.</p>
+                        <div class="d-flex">
+                            <h4>AHI SALMON NIGIRI</h4>
+                            <p>$48</p>
+                        </div>
+                        <p>Eget vulputate vitae quis rutrum blandit <br>sed. Quam nulla sit lacinia.</p>
+                        <div class="d-flex">
+                            <h4>AHI SALMON NIGIRI</h4>
+                            <p>$48</p>
+                        </div>
+                        <p>Eget vulputate vitae quis rutrum blandit <br>sed. Quam nulla sit lacinia.</p>
+                    </div>
+                    <div class="col-4 main_eight_two">
+                        <img src="../assets/images/misc22x.jpg" alt="">
+                        <div class="text_main_eight_center"> 
+                            <h2>HORS D'OEUVRES</h2>
+                        <div class="d-flex">
+                            <h4>AHI SALMON NIGIRI</h4>
+                            <p class="prezzo">$48</p>
+                        </div>
+                        <p>Eget vulputate vitae quis rutrum blandit <br>sed. Quam nulla sit lacinia.</p>
+                        <div class="d-flex">
+                            <h4>AHI SALMON NIGIRI</h4>
+                            <p class="prezzo">$48</p>
+                        </div>
+                        <p>Eget vulputate vitae quis rutrum blandit <br>sed. Quam nulla sit lacinia.</p>
+                        <div class="d-flex">
+                            <h4>AHI SALMON NIGIRI</h4>
+                            <p class="prezzo">$48</p>
+                        </div>
+                        <p>Eget vulputate vitae quis rutrum blandit <br>sed. Quam nulla sit lacinia.</p>
+                        <div class="d-flex">
+                            <h4>AHI SALMON NIGIRI</h4>
+                            <p class="prezzo">$48</p>
+                        </div>
+                        <p>Eget vulputate vitae quis rutrum blandit <br>sed. Quam nulla sit lacinia.</p>
+                        </div>
+                    </div>
+                    <div class="col-4 main_eight_three">
+                        <h2>HORS D'OEUVRES</h2>
+                        <div class="d-flex">
+                            <h4>AHI SALMON NIGIRI</h4>
+                            <p>$48</p>
+                        </div>
+                        <p>Eget vulputate vitae quis rutrum blandit <br>sed. Quam nulla sit lacinia.</p>
+                        <div class="d-flex">
+                            <h4>AHI SALMON NIGIRI</h4>
+                            <p>$48</p>
+                        </div>
+                        <p>Eget vulputate vitae quis rutrum blandit <br>sed. Quam nulla sit lacinia.</p>
+                        <div class="d-flex">
+                            <h4>AHI SALMON NIGIRI</h4>
+                            <p>$48</p>
+                        </div>
+                        <p>Eget vulputate vitae quis rutrum blandit <br>sed. Quam nulla sit lacinia.</p>
+                        <div class="d-flex">
+                            <h4>AHI SALMON NIGIRI</h4>
+                            <p>$48</p>
+                        </div>
+                        <p>Eget vulputate vitae quis rutrum blandit <br>sed. Quam nulla sit lacinia.</p>
+                    </div>
+
+                </div>
+            </div>
+        </div>
   </div>
 </template>
 
@@ -245,7 +384,7 @@ export default {
                         margin-top: 40px;
                         margin-left: 120px;
                     }
-                    .button_main_top_tight {
+                    .button_main_top_right {
                         color: white;
                         background: black;
                         border: 1px solid white;
@@ -263,25 +402,261 @@ export default {
         margin-left: 180px;
         margin-top: 60px;
         .main_fourth_left {
-            img {
-                width: 520px;
-                height: 600px;
-                position: relative;
-                margin-right: 40px;
-                filter: brightness(50%);
-                
+            position: relative;
+            .main_fourth_img_left {
+                img {
+                    width: 520px;
+                    height: 630px;
+                    margin-right: 40px;
+                    filter: brightness(35%);
+                }
             }
+            .main_fourth_text {
+                position: absolute;
+                top: 100px;
+                p {
+                    color: #d5ddd8 ;
+                    margin-left: 120px;
+                    line-height: 30px;
+                }
+                h2 {
+                    color: white;
+                    margin-left: 120px;
+                }
+                .stars {
+                    color: white;
+                    margin-left: 120px;
+                    margin-top: 30px;
+                    margin-bottom: 30px;
+                }
+                .line_main_third {
+                    border-top: 1px solid white;
+                    width: 60px;
+                    margin-left: 120px;
+                    margin-top: 40px;
+                    margin-bottom:40px;
+                }
+                .button_main_top_right {
+                    color: white;
+                    background: rgba(0, 0, 0, 0);
+                    border: 1px solid white;
+                    margin-left: 120px;
+                    margin-top: 30px;
+                    width: 212px;
+                    height: 39px;
+                }
+            }
+            
         }
         .main_fourth_right {
+            position: relative;
             img {
                 width: 520px;
-                position: relative;
-                height: 600px;
-                filter: brightness(50%);
+                height: 630px;
+                filter: brightness(35%);
                 
+            }
+            .main_fourth_text {
+                position: absolute;
+                top: 100px;
+                p {
+                    color: #d5ddd8 ;
+                    margin-left: 120px;
+                    line-height: 30px;
+                }
+                h2 {
+                    color: white;
+                    margin-left: 120px;
+                }
+                .stars {
+                    color: white;
+                    margin-left: 120px;
+                    margin-top: 30px;
+                    margin-bottom: 30px;
+                }
+                .line_main_third {
+                    border-top: 1px solid white;
+                    width: 60px;
+                    margin-left: 120px;
+                    margin-top: 40px;
+                    margin-bottom:40px;
+                }
+                .button_main_top_right {
+                    color: white;
+                    background: rgba(0, 0, 0, 0);
+                    border: 1px solid white;
+                    margin-left: 120px;
+                    margin-top: 30px;
+                    width: 212px;
+                    height: 39px;
+                }
             }
         }
 
+    }
+    .main_five {
+        position: relative;
+        img {
+            width: 1452px;
+            margin-top: 90px;
+            height: 450px;
+            filter: brightness(90%);
+        }
+        .main_five_text {
+            position: absolute;
+            top: 510px;
+            margin-left: 640px;
+            font-size: 13px;
+            color: white;
+        }
+    }
+    .main_six {
+        h2 {
+            margin-top: 70px;
+            margin-bottom: 50px;
+
+        }
+        .main_six_images {
+            width: 75%;
+            margin: auto;
+        }
+        
+        img {
+            width: 100%;
+            height: 430px;
+        }
+        p {
+            font-size: 8px;
+
+        }
+        h3 {
+            font-size: 20px;
+            margin-top: 30px;
+        }
+        
+    }
+    .main_seven {
+        margin-top: 100px;
+        .col-7 {
+            padding-right: 0px;
+            img {
+                width: 100%;
+                height: 700px;
+            }
+        }
+        .col-5 {
+            background: black;
+            .main_seven_text_top {
+                margin-left: 60px;
+                color: #9BA59E;
+                margin-top: 150px;
+                margin-bottom: 30px;
+            }
+            .line {
+                border-top: 1px solid #9BA59E;;
+                width: 45px;
+                margin-top: 8px;
+                margin-right: 35px;
+                }
+            .text_main_top_right {
+                color: #9BA59E;
+                font-size: 15px;
+                margin-left: 140px;
+                margin-bottom: 70px;
+                line-height: 25px;
+                }
+            h2 {
+                color: white;
+                margin-left: 140px;
+                margin-bottom: 30px;
+            }
+            button {
+                margin-left: 140px;
+                background: black;
+                color: white;
+                width: 221px;
+                height: 40px;
+                border: 1px solid white;
+                font-size: 13px;
+            }
+        }
+    }
+    .main_eight {
+        .main_eight_one {
+            background: #E8E8E8;
+            margin-top: 150px;
+            h2 {
+                margin-left: 40px;
+                margin-top: 80px;
+                margin-bottom: 60px;
+            }
+            .d-flex {
+                margin-left: 40px;
+                justify-content: space-between;
+            }
+            p {
+                margin-left: 40px;
+                margin-bottom: 30px;
+            }
+            
+
+        }
+        .main_eight_two {
+            padding-left: 0px;
+            padding-right: 0px;
+            margin-top: 100px;
+            position: relative;
+            .text_main_eight_center {
+                position: absolute;
+                top: 0;
+                h2 {
+                margin-left: 40px;
+                margin-top: 80px;
+                margin-bottom: 60px;
+                color: white;
+                }
+                .d-flex {
+                margin-left: 40px;
+                justify-content: space-between;
+                }
+                p {
+                margin-left: 40px;
+                margin-bottom: 30px;
+                color: white;
+                }
+                .prezzo {
+                    margin-left: 120px;
+                }
+                h4 {
+                    color: white;
+                }
+            }
+            img {
+                width: 100%;
+                filter: brightness(35%);
+                height: 704px;
+                
+            }
+
+        }
+        .main_eight_three {
+            background: #E8E8E8;
+            margin-top: 150px;
+            h2 {
+                margin-left: 40px;
+                margin-top: 80px;
+                margin-bottom: 60px;
+            }
+            .d-flex {
+                margin-left: 40px;
+                justify-content: space-between;
+            }
+            p {
+                margin-left: 40px;
+                margin-bottom: 30px;
+            }
+
+        }
     }
     
 }
